6bb00b4f0e92c45f04df0d2e0d0f01b53a8046ef
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2021-11-08 22:27:29 +0300

    console

---
+++ console.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+import cmd, sys
+
+class ConsoleShell(cmd.Cmd):
+    intro = 'Welcome to the AirBnB clone. Type help or ? to list commands. \n'
+    prompt = '(hbnb) '
+    file = None
+
+    def do_EOF(self, arg):
+        'Check if it is End-Of-File: EOF'
+        # something to be done here
+        print('EOF')
+
+
+    def do_quit(self, arg):
+        'close the turtle window, and exit:  EXIT'
+        print('Thank you for AirBnB clone shell;-)')
+        self.close()
+        # bye()
+        return True
+
+    def close(self):
+        if self.file:
+            self.file.close()
+            self.file = None
+
+if __name__ == '__main__':
+    ConsoleShell().cmdloop()
